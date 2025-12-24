34e73a069dee303f2d12f85541a4307eb34cb0c7
Marco Bonici <bonici.marco@gmail.com>
2025-12-24 09:39:01 -0500

    More trained emu

--- src/Effort.jl
+++ src/Effort.jl
@@ -36,6 +36,16 @@ function __init__()
     trained_emulators["PyBirdmnuw0wacdm"]["0"] = load_multipole_emulator(joinpath(artifact"PyBirdmnuw0wacdm", "0/"))
     trained_emulators["PyBirdmnuw0wacdm"]["2"] = load_multipole_emulator(joinpath(artifact"PyBirdmnuw0wacdm", "2/"))
     trained_emulators["PyBirdmnuw0wacdm"]["4"] = load_multipole_emulator(joinpath(artifact"PyBirdmnuw0wacdm", "4/"))
+
+    trained_emulators["VelocileptorsREPTmnuw0wacdm"] = Dict()
+    trained_emulators["VelocileptorsREPTmnuw0wacdm"]["0"] = load_multipole_emulator(joinpath(artifact"trained_effort_velocileptors_rept_mnuw0wacdm", "0/"))
+    trained_emulators["VelocileptorsREPTmnuw0wacdm"]["2"] = load_multipole_emulator(joinpath(artifact"trained_effort_velocileptors_rept_mnuw0wacdm", "2/"))
+    trained_emulators["VelocileptorsREPTmnuw0wacdm"]["4"] = load_multipole_emulator(joinpath(artifact"trained_effort_velocileptors_rept_mnuw0wacdm", "4/"))
+
+    trained_emulators["VelocileptorsLPTmnuw0wacdm"] = Dict()
+    trained_emulators["VelocileptorsLPTmnuw0wacdm"]["0"] = load_multipole_emulator(joinpath(artifact"trained_effort_velocileptors_lpt_mnuw0wacdm", "0/"))
+    trained_emulators["VelocileptorsLPTmnuw0wacdm"]["2"] = load_multipole_emulator(joinpath(artifact"trained_effort_velocileptors_lpt_mnuw0wacdm", "2/"))
+    trained_emulators["VelocileptorsLPTmnuw0wacdm"]["4"] = load_multipole_emulator(joinpath(artifact"trained_effort_velocileptors_lpt_mnuw0wacdm", "4/"))
 end
 
 include("neural_networks.jl")
